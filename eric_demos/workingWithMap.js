98713f1a0ff7bca9f27836090d1f5f0c9bb5288f
Wilfredo Contreras <wilfredojrcontreras@gmail.com>
2024-05-15 14:17:43 -0500

    more of erics demos

--- eric_demos/workingWithMap.js
+++ eric_demos/workingWithMap.js
@@ -6,6 +6,13 @@ let kids = [
     { first: "Zachary", last: "Westly" }
 ];
 
+
+let arrayOfFirstNames = kids.map((kid) => {
+    return kid.first;
+})
+
+console.log(arrayOfFirstNames);
+
 // .map is working with the data, .filter is like a true or false statement thing
 let newArrayOfKidsFullNames = kids.map((kid) => {
 
@@ -15,11 +22,15 @@ let newArrayOfKidsFullNames = kids.map((kid) => {
 
 console.log(newArrayOfKidsFullNames);
 
+// [ get a new list of an object with a new key (property) of fullName added ]
+// [ Full name combines the first and last properties to get it's value ]
 let newArrayOfKidsWithFullNames = kids.map((kid) => {
 
     // returns a new object
-    return {...kid, fullName: `${kid.first} ${kid.last}`}
-
+    return { ...kid, fullName: `${kid.first} ${kid.last}` } /* [ the "...kid," is a spread operator, takes two 
+                                                            properties and adds them into one new property ]
+                                                            */
+    
 })
 
 console.log(newArrayOfKidsWithFullNames);
